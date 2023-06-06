3b834178c2168841c2fa4aa009347109975b1df3
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-06 19:14:09 +0900

    post action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,8 +19,27 @@ class PostsController < ApplicationController
       render :new      
     end    
   end
+  def show
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+  end
 
-    
+  def edit
+    @post = Post.find(params[:id])
+    unless  @post.user_id == current_user.id
+      redirect_to action: :index      
+    end
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+   
   private
   
   def post_params
