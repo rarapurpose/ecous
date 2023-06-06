dfaf054e3095f5a888cc496e19579e0321220850
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-06 17:07:32 +0900

    user setup

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,10 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
+
+  private  
+  
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])    
+  end
+
 end
