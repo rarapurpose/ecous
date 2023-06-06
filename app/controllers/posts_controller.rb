890ad87bddb81a205a13540960ff20f5eeaefb2d
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-06 20:03:34 +0900

    modify /before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
-class PostsController < ApplicationController
-  before_action :authenticate_user!
+class PostsController < ApplicationController 
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @posts = Post.all
@@ -19,6 +20,7 @@ class PostsController < ApplicationController
       render :new      
     end    
   end
+
   def show
     @post = Post.find(params[:id])
     @comment = Comment.new
@@ -45,11 +47,17 @@ class PostsController < ApplicationController
      @post.destroy
     redirect_to new
   end
-   
+  
   private
   
   def post_params
     params.require(:post).permit(:title, :detail, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 
 end
