838eb5127cfc6fc6581536f0617812895878bc03
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-06 19:39:15 +0900

    comment setup

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,6 +39,12 @@ class PostsController < ApplicationController
       render :edit
     end
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+     @post.destroy
+    redirect_to new
+  end
    
   private
   
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
   
-  resources :posts 
+  resources :posts do
+  end
+  
 
   resources :users, only: :show
 end
