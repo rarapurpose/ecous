82f70924eb4c2a8605076928ced091cee4e7584d
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-06 19:03:10 +0900

    post function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!
 
   def index
     @posts = Post.all
@@ -7,5 +8,23 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save
+      redirect_to root_path
+    else
+      @post = Post.new(post_params)
+      render :new      
+    end    
+  end
+
+    
+  private
   
+  def post_params
+    params.require(:post).permit(:title, :detail, :image).merge(user_id: current_user.id)
+  end
+
 end
