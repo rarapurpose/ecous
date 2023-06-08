87670ab0f2981fe83608f5856af3552b4210842b
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-08 21:13:55 +0900

    Update comments_controller.rb

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,22 +1,22 @@
 class CommentsController < ApplicationController
 
-    def create
-      @comment = Comment.new(comment_params)
-      if @comment.save
-        redirect_to post_path(@comment.post)
-      else
-        @post = @comment.post
-        @comments = @post.comments
-         render "posts/show"
-      end    
-    end
-  
-    private
-  
-    def comment_params
-      params.require(:comment).permit(:content, :post, :user).merge(user_id: current_user.id, post_id: params[:post_id])
-    end
+  def create
+   @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@comment.post)
+    else
+      @post = @comment.post
+      @comments = @post.comments
+      render "posts/show"
+    end    
   end
+  
+  private
+   
+  def comment_params
+    params.require(:comment).permit(:content, :post, :user).merge(user_id: current_user.id, post_id: params[:post_id])
+   end
+   
 end
 
 
