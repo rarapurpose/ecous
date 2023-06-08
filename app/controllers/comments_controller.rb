c58b64bf91db52368174bacd8eadb09cb01371f0
rarapurpose <129743144+rarapurpose@users.noreply.github.com>
2023-06-08 20:41:51 +0900

    render fix

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -16,6 +16,7 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:content, :post, :user).merge(user_id: current_user.id, post_id: params[:post_id])
     end
-  
   end
 end
+
+
